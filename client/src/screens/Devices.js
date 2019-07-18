74c10cb997850a080b4d86d1a0bc92ccc77bed81
Edie "Danger" Woelfle <edie.woelfle@gmail.com>
2019-07-17 22:32:44 -0400

    getting a basic table - using hardcoded data for starters

---
+++ client/src/screens/Devices.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export const Devices = ({ devices }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Device ID</th>
+          <th>Firmware</th>
+          <th>Date Available</th>
+          <th>Battery Level</th>
+          <th>Temp</th>
+        </tr>
+      </thead>
+      <tbody>
+        {devices.map(device => (
+          <DeviceRow device={device} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const DeviceRow = device => {
+  const {
+    application_code,
+    asset_identifier,
+    battery_level,
+    date_device_available,
+    device_id,
+    firmware_version,
+    id,
+    internal_temperature,
+    manufacturer,
+    status
+  } = device.device;
+
+  console.log("deviceId", device_id);
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{device_id}</td>
+      <td>{firmware_version}</td>
+      <td>{date_device_available}</td>
+      <td>{battery_level}</td>
+      <td>{internal_temperature}</td>
+    </tr>
+  );
+};
